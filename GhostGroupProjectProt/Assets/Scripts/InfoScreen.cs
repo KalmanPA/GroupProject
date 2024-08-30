7e971c0a4236c2774e410f121b10ca0455c00b9a
Kálmán Pajcsics <105979789+KalmanPA@users.noreply.github.com>
2024-08-30 15:16:00 +0200

    Update InfoScreen.cs

--- GhostGroupProjectProt/Assets/Scripts/InfoScreen.cs
+++ GhostGroupProjectProt/Assets/Scripts/InfoScreen.cs
@@ -6,7 +6,7 @@ public class InfoScreen : MonoBehaviour
 {
     private static bool _wasShownOnce = true;
 
-    private bool isPaused = true; // Track if the game is paused
+    private bool _isPaused = true; // Track if the game is paused
 
     [SerializeField] private GameObject _infoScreen;
 
@@ -14,19 +14,18 @@ public class InfoScreen : MonoBehaviour
     {
         InputReader.InfoEvent += InputReader_InfoEvent;
 
-        isPaused = _wasShownOnce;
+        _isPaused = _wasShownOnce;
         _infoScreen.SetActive(_wasShownOnce);
 
         if (_wasShownOnce)
         {
             PauseGame();
         }
-
     }
 
     private void InputReader_InfoEvent()
     {
-        if (isPaused)
+        if (_isPaused)
         {
             ResumeGame();
             _infoScreen.SetActive(false);
@@ -43,7 +42,7 @@ public class InfoScreen : MonoBehaviour
     private void PauseGame()
     {
         Time.timeScale = 0;  // Set the time scale to 0 to pause the game
-        isPaused = true;     // Set the pause state to true
+        _isPaused = true;     // Set the pause state to true
         // Optionally: Disable any UI elements or scripts that should not be active when paused
     }
 
@@ -51,7 +50,7 @@ public class InfoScreen : MonoBehaviour
     private void ResumeGame()
     {
         Time.timeScale = 1;  // Set the time scale to 1 to resume the game
-        isPaused = false;    // Set the pause state to false
+        _isPaused = false;    // Set the pause state to false
         // Optionally: Re-enable any UI elements or scripts that were disabled
     }
 
